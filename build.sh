949e67222dd1d6d39bbab404e6575ad9c407df68
Michael <68412795+TheMasterOfMike@users.noreply.github.com>
2023-10-27 17:41:09 -0500

    switch to v3.12/community because for some reason gcompat isn't on edge/community

--- build.sh
+++ build.sh
@@ -117,7 +117,7 @@ mount -vt proc proc rootfs/proc
 cp /etc/resolv.conf rootfs/etc
 cat << ! > rootfs/etc/apk/repositories
 http://dl-cdn.alpinelinux.org/alpine/v3.12/main
-http://dl-cdn.alpinelinux.org/alpine/edge/community
+http://dl-cdn.alpinelinux.org/alpine/v3.12/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 !
 
